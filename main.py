a7e125788d06a7b5855b86950655f29d2be3d785
chopchaper42 <vorongri@fel.cvut.cz>
2024-04-07 14:22:09 +0200

    dashboard finished

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
+
+username = "username"
 
 
 @app.route('/<anything>')
 def hello_world(anything):  # put application's code here
-    return render_template("hello.html")
+    return redirect(url_for('login'))
 
 
 @app.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template("login.html", user=username)
 
 
 @app.route('/registration')
 def registration():
-    return render_template("registration.html")
+    return render_template("registration.html", user=username)
 
 
 @app.route('/dashboard')
@@ -36,7 +38,9 @@ def dashboard():
         "4-3-2024 13:00:00": 8.6,
         "4-3-2024 18:00:00": 7.7
     }
-    return render_template("dashboard.html", data=temp_map)
+    temps_number = len(list(temp_map.keys()))
+    last_temp = list(temp_map.keys())[len(temp_map) - 1]
+    return render_template("dashboard.html", data=temp_map, user=username, last_temp=last_temp, t_number=temps_number)
 
 
 if __name__ == '__main__':

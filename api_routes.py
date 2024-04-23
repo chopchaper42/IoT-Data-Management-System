7a079c85ef614d268765ad085086f1dd403b31f1
chopchaper42 <vorongri@fel.cvut.cz>
2024-04-23 19:38:09 +0200

    API routes added in separate file

---
+++ api_routes.py
@@ -0,0 +1,40 @@
+from __main__ import app
+from data import Data
+
+data = Data()
+
+
+# Vložení hodnoty {“timestamp”: value, “temp”: value}
+@app.route('/api/add/<value>', methods=['POST'])
+def add(value):
+    data.add(value)
+    return data.temps
+
+
+# Získání poslední hodnoty {“timestamp”: value, “temp”: value}
+
+@app.route('/api/last/')
+def last():
+    return data.last()
+
+
+# Získání posledních X naměřených hodnot.
+
+@app.route('/api/last/<n>')
+def last_n(n):
+    return data.last_n(n)
+
+
+# Smazání nejstarších Y naměřených hodnot.
+
+@app.route('/api/delete/<n>')
+def delete_n(n):
+    data.del_n_oldest(n)
+    return data.temps
+
+
+# Ziskani vsech hodnot
+
+@app.route('/api/get_all_temps')
+def get_all_temps():
+    return data.temps
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, redirect, url_for
+from data import Data
 
 app = Flask(__name__)
 
-username = "username"
+import api_routes
 
+username = "username"
+data = Data()
 
 @app.route('/<anything>')
 def hello_world(anything):  # put application's code here
@@ -22,26 +25,32 @@ def registration():
 
 @app.route('/dashboard')
 def dashboard():
-    temp_map = {
-        "21-2-2024 13:00:00": 9.5,
-        "21-2-2024 18:00:00": 6.5,
-        "23-2-2024 13:00:00": 11.0,
-        "23-2-2024 18:00:00": 9.3,
-        "25-2-2024 13:00:00": 9.9,
-        "25-2-2024 18:00:00": 10.7,
-        "27-2-2024 13:00:00": 10.3,
-        "27-2-2024 18:00:00": 4.3,
-        "29-2-2024 13:00:00": 14.7,
-        "29-2-2024 18:00:00": 15.3,
-        "2-3-2024 13:00:00": 13.9,
-        "2-3-2024 18:00:00": 15.4,
-        "4-3-2024 13:00:00": 8.6,
-        "4-3-2024 18:00:00": 7.7
-    }
-    temps_number = len(list(temp_map.keys()))
-    last_temp = list(temp_map.keys())[len(temp_map) - 1]
-    return render_template("dashboard.html", data=temp_map, user=username, last_temp=last_temp, t_number=temps_number)
+    temps_number = len(data.temps)
+    last_temp = data.temps[len(data.temps) - 1]
+    return render_template("dashboard.html", data=data.temps, user=username, last_temp=last_temp["value"], t_number=temps_number)
 
 
 if __name__ == '__main__':
+    # temp_map = {
+    #     "21-2-2024 13:00:00": 9.5,
+    #     "21-2-2024 18:00:00": 6.5,
+    #     "23-2-2024 13:00:00": 11.0,
+    #     "23-2-2024 18:00:00": 9.3,
+    #     "25-2-2024 13:00:00": 9.9,
+    #     "25-2-2024 18:00:00": 10.7,
+    #     "27-2-2024 13:00:00": 10.3,
+    #     "27-2-2024 18:00:00": 4.3,
+    #     "29-2-2024 13:00:00": 14.7,
+    #     "29-2-2024 18:00:00": 15.3,
+    #     "2-3-2024 13:00:00": 13.9,
+    #     "2-3-2024 18:00:00": 15.4,
+    #     "4-3-2024 13:00:00": 8.6,
+    #     "4-3-2024 18:00:00": 7.7
+    # }
+    # new_temps = []
+    # for temp in temp_map:
+    #     new_temps.append({"timestamp": temp, "temp": temp_map[temp]})
+    #
+    # with open('temps.json', 'w') as f:
+    #     json.dump(new_temps, f)
     app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
